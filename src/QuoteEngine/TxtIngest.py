b3fdce9f1adbcf4718e189ea28dea747f41e057c
Claudia Hill <hillc255@yahoo.com>
2022-06-02 03:58:31 -0400

    update csv and txt ingest files

---
+++ src/QuoteEngine/TxtIngest.py
@@ -0,0 +1,43 @@
+from typing import List
+from IngestorInterface import IngestorInterface
+from QuoteModel import QuoteModel
+
+
+class TxtIngestor(IngestorInterface):
+
+    allowed_file_extention = ['txt']
+    
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            raise Exception('cannot ingest exception')
+
+        quoteList = []
+
+        with open(path, encoding='utf8') as f:
+
+            for line in f:
+                line = line.strip('\n\n\r').strip().replace(" - ",",").replace(u'\ufeff', '')
+                body, author = tuple(line.split(','))
+                quoteList.append(QuoteModel(body, author))
+        return quoteList
+       
+if __name__ == "__main__":
+    
+    quoteList = (TxtIngestor.parse('./_data/DogQuotes/DogQuotesTXT.txt'))
+    print(quoteList)
+
+
+
+#The project contains a TextIngestor class.
+#The class inherits the IngestorInterface.
+#The class does not depend on any 3rd party library to complete the defined, abstract method signatures to parse Text files.
+#The parse method returns a valid QuoteModel
+
+
+#hillc@LAPTOP-C1T59ERH MINGW64 ~/meme/src (main)
+#$ python QuoteEngine/TxtIngest.py
+
+#results in exception
+#quoteList = (CSVIngestor.parse('./_data/DogQuotes/DogQuotesPDF.pdf'))
+#print(quoteList)
