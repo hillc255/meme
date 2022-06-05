bcdf922ff8118a476af9c29e9dc5719ed23b6d8f
Claudia Hill <hillc255@yahoo.com>
2022-06-05 12:46:08 -0400

    update pycodestyle

--- src/QuoteEngine/IngestorInterface.py
+++ src/QuoteEngine/IngestorInterface.py
@@ -1,19 +1,27 @@
+"""IngestorInterface module provides generic abstract base classes."""
+
 from QuoteModel import QuoteModel
 from abc import ABC, abstractmethod
 from typing import List
 
 
-class IngestorInterface(ABC):   
-    """IngestorInterface class for Abstract Base Class."""
+class IngestorInterface(ABC):
+    """IngestorInterface class for generic classmethods.
+
+    Class has a classmethod for parsing of files and
+    extracting 'body' and 'author' for a QuoteModel object.
+    """
 
     allowed_file_extension: List[str] = []
 
     @classmethod
     def can_ingest(cls, path: str):
+        """Can_ingest classmethod to determine allowed_file_extension."""
         extension = path.split('.')[-1]
         return extension in cls.allowed_file_extension
 
     @classmethod
     @abstractmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        """Parse abstract method used for strategy object parsing."""
         pass
