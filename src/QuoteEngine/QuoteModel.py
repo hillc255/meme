eab6756123e9e2ff8861b7a1bfdc471930533a80
Claudia Hill <hillc255@yahoo.com>
2022-06-05 13:30:06 -0400

    update pycodestyle

--- src/QuoteEngine/QuoteModel.py
+++ src/QuoteEngine/QuoteModel.py
@@ -1,16 +1,25 @@
+"""QuoteModel model for body and author text encapsulation."""
 
-
-"""
-Provides QuoteModel with body and author models
-"""
 
 class QuoteModel:
+    """QuoteModel class with body and author parameters.
+
+    Args:
+        body (str): Quote which forms the body.
+        author (str): Name of author of the quote.
+    Returns:
+        QuoteModel: Object with body and author.
+    """
+
     def __init__(self, body: str, author: str):
+        """Initiate string parameters body and author."""
         self.body = body
         self.author = author
 
     def __repr__(self):
+        """Representation of an object string."""
         return f"Body: {self.body} - Author: {self.author}"
 
     def __str__(self):
+        """Print out class representation as a string."""
         return f"Body: {self.body} - Author: {self.author}"
