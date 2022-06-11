5fe5bf4451b48fe4ae11d30274f96ebf9b337c66
Claudia Hill <hillc255@yahoo.com>
2022-06-11 13:05:52 -0400

    update pydocstyle and pycodestyle

--- src/MemeEngine/MemeEngine.py
+++ src/MemeEngine/MemeEngine.py
@@ -1,4 +1,5 @@
-import typing
+"""MemeEngine module provides MemeEngine class with methods to make a meme."""
+
 from typing import Optional
 from PIL import Image, ImageDraw, ImageFont
 import os
@@ -6,40 +7,46 @@ import random
 from random import randint
 import textwrap
 
-#class for the memeengine
+
 class MemeEngine:
+    """MemeEngine class to make a meme.
+
+    Method parameters are to make the meme and save it to
+    a given path.
+    """
+
     def __init__(self, output_dir):
+        """Init method with required output_dir argument to save the image.
+
+        Argument:
+        output_dir (str) : Required destination path to save the meme
+        """
         self.output_dir = output_dir
 
-    #method
-    def make_meme(self, img_path: str, body: str, author: str, width=500) -> Optional[str]:
-        # loads the image from the imgPath, creating an in-memory represtation of the image
-        
+    def make_meme(self, img_path: str, body: str,
+                  author: str, width=500) -> Optional[str]:
+        """Make_meme method signature to make the meme with required arguments.
+
+        Arguments:
+        img_path (str)   : Required path to load the image
+        body (str)       : Required quote from QuoteModel
+        author (str)     : Required author from QuoteModel
+        width (int)      : Size of the image
+        """
         try:
             img: Image.Image = Image.open(img_path, mode='r')
         except Exception:
             raise OSError('Unable to open file')
 
-        print(f"img: ",img)
-        print(f"body: ",body)
-        print(f"author: ",author)
-
-
-        # create a destination path for the meme
-
-        outputFile_name = os.path.join(self.output_dir, 
-            f'meme_{random.randint(0,100000000)}.png')
-
-        print(f"outputFile_name:  {outputFile_name}")
+        outputFile_name = os.path.join(
+            self.output_dir, f'meme_{random.randint(0,100000000)}.png')
 
         if width is not None:
             ratio = width/float(img.size[0])
             height = int(ratio*float(img.size[1]))
             img = img.resize((width, height), Image.NEAREST)
 
-        # Add text and author test to the image
-        message = (str(body) + " - "+ str(author))
-        print(f"message: ", message)
+        message = (str(body) + " - " + str(author))
 
         if message is not None:
             draw = ImageDraw.Draw(img)
@@ -49,47 +56,17 @@ class MemeEngine:
                     'fonts/LilitaOne-Regular.ttf'
                 )
 
-            try:    
+            try:
                 font = ImageFont.truetype(ttfFile, size=30)
             except Exception:
                 raise OSError('Invalid font path')
 
-            #wrapped message by message char length
             message = textwrap.fill(text=message, width=20)
 
-            #generate random x,y coordinates
-            draw.text(((random.randint(20,250)), (random.randint(20,250))), message, font=font, fill='white')
+            draw.text(((random.randint(20, 250)),
+                      (random.randint(20, 250))), message,
+                      font=font, fill='white')
 
-        print("After message and ImageDraw")
-        print("After message and ImageDraw")
-
-        # save the meme to the dest path
         img.save(outputFile_name)
 
-        print("Saved image")
-
         return os.path.relpath(path=outputFile_name, start=os.curdir)
-        #return os.path.abspath(outputFile_name)
-        
-
-#Requirements:
-#The project defines a MemeGenerator module with the following 
-# responsibilities:
-
-#Loading of a file from disk
-#Transform image by resizing to a maximum width of 500px while maintaining the 
-# input aspect ratio
-
-#Add a caption to an image (string input) with a body and author to a random 
-# location on the image.
-
-#The class depends on the Pillow library to complete the defined, incomplete method 
-# signatures so that they work with JPEG/PNG files.
-
-#The method signature to make the meme should be: make_meme(self, img_path, text, 
-# author, width=500) -> str #generated image path
-
-#The init method should take a required argument for where to save the generated 
-# images: __init__(self, output_dir...).
-
-
